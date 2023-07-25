0f3b0ff03e3123cd6f88feba7ef2d48b1f688007
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-25 11:30:37 -0500

    update

--- src/main/java/com/example/demo2/Controller/UserController.java
+++ src/main/java/com/example/demo2/Controller/UserController.java
@@ -4,7 +4,7 @@ import com.example.demo2.Dto.UserDTO;
 import com.example.demo2.Entity.User;
 import com.example.demo2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,12 +13,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    public void save(UserDTO user) {
+    @PostMapping("/users")
+    public void save(@RequestBody UserDTO user) {
 
     }
-
+    @GetMapping("/users")
     public List<User> getAll() {
         return null;
     }
 
+    @GetMapping("/users/{id}")
+    public UserDTO getById(@PathVariable int id) {
+        return null;
+    }
+
+    @PutMapping("/users/{id}")
+    public void update(@RequestBody UserDTO user, @PathVariable int id) {
+
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable int id) {
+
+    }
 }
